0255e520af14052d36a6c3dfe2786ff12c15f669
George Vlachos <gvlachos@users.noreply.github.com>
2021-09-22 17:31:21 +0300

    add angular-material with a prebuild theme

--- projects/dynamic-form-demo/src/app/app.module.ts
+++ projects/dynamic-form-demo/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DynamicFormModule } from '@dynamic-form-src';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DynamicFormModule],
+  imports: [BrowserAnimationsModule, BrowserModule, DynamicFormModule],
   providers: [],
   bootstrap: [AppComponent],
 })
