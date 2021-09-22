41bf60a7d4303d06d144cc80ef14549d2e0370a9
George Vlachos <gvlachos@users.noreply.github.com>
2021-09-22 17:33:45 +0300

    add a material modules with the components used, add the UI components implementations

---
+++ projects/dynamic-form/src/lib/dynamic-form-material.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+const passThroughArray = [
+  CommonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+];
+
+@NgModule({
+  imports: passThroughArray,
+  exports: passThroughArray,
+})
+export class DynamicFormMaterialModule {}
--- projects/dynamic-form/src/lib/dynamic-form.module.ts
+++ projects/dynamic-form/src/lib/dynamic-form.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxErrorsModule } from '@ngspot/ngx-errors';
+import { DynamicFormMaterialModule } from './dynamic-form-material.module';
 import { DynamicReactiveFieldComponent } from './dynamic-reactive-field/dynamic-reactive-field.component';
 import { DynamicReactiveFormComponent } from './dynamic-reactive-form/dynamic-reactive-form.component';
 
 @NgModule({
   declarations: [DynamicReactiveFormComponent, DynamicReactiveFieldComponent],
-  imports: [CommonModule, BrowserAnimationsModule, NgxErrorsModule, ReactiveFormsModule],
+  imports: [CommonModule, BrowserAnimationsModule, DynamicFormMaterialModule, NgxErrorsModule, ReactiveFormsModule],
   exports: [DynamicReactiveFormComponent],
 })
 export class DynamicFormModule {}
