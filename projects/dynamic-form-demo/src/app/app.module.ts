59eb1edec490ea2fb047a4e7027a3555bad6fd62
George Vlachos <gvlachos@users.noreply.github.com>
2021-09-23 09:05:39 +0300

    update app module and add tui-root in the app component

--- projects/dynamic-form-demo/src/app/app.module.ts
+++ projects/dynamic-form-demo/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DynamicFormModule } from '@dynamic-form-src';
-
+import { TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DynamicFormModule],
+  imports: [BrowserModule, DynamicFormModule, TuiRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
